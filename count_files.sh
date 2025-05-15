d2a63a49a7350b6420876dc67948f52be0e33da3
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-15 17:30:06 +0100

    commit

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/bash 
 # Demander à l'utilisateur de saisir un nom de dossier
 echo "Veuillez entrer le nom d'un dossier :"
 read dossier
@@ -9,11 +8,10 @@ if [ ! -d "$dossier" ]; then
     echo "Erreur : le dossier '$dossier' n'existe pas."
     exit 1
 fi
-
-# Utiliser ls et wc -l pour compter les fichiers (non récursif)
-# On filtre avec 'ls -p' qui ajoute un '/' aux dossiers
-# Puis on enlève les lignes qui se terminent par '/' (donc les dossiers)
-nb_fichiers=$(ls -p "$dossier" | grep -v '/$' | wc -l)
-
-# Afficher le résultat
-echo "Le dossier $dossier contient $nb_fichiers fichier(s)."
+    # Utiliser ls et wc -l pour compter les fichiers (non récursif)
+    # On filtre avec 'ls -p' qui ajoute un '/' aux dossiers
+    # Puis on enlève les lignes qui se terminent par '/' (donc les dossiers)
+  nb_fichiers=$( ls -p "$dossier" | grep -v '/$'| wc -l)
+    # Afficher le résultat
+  echo "Le dossier $dossier contient $nb_fichiers fichier(s)."
+ 
