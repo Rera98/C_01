eb23ca7dcc03c11d91028e62c779d609a5ab793d
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-16 11:09:43 +0100

    commit

--- count_files.sh
+++ count_files.sh
@@ -5,14 +5,7 @@ echo "Entrez le nom du dossier :"
 read dossier
 
 # Vérifie si le dossier existe
-if [ -d "$dossier" ]; then
-    # Utilise ls pour lister les éléments visibles, puis trouve ceux qui sont des fichiers
-    #nb_fichiers=$(ls "$dossier" | while read item; do
-        #if [ -f "$dossier/$item" ]; then
-           # echo "$item"
-        #fi
-    #done | wc -l)
-    # Affiche le nombre de fichier 
+if [ -d "$dossier" ]; then 
     nb_fichiers=$(ls $dossier | wc -l ) 
     echo "Le dossier $dossier contient $nb_fichiers fichier(s)."
 else
