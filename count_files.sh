671483b51821771c7dbcdf11e323126a7c4294d1
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-16 16:46:34 +0100

    commit

--- count_files.sh
+++ count_files.sh
@@ -9,5 +9,5 @@ if [ -d "$dossier" ]; then
     nb_fichiers=$( ls $dossier | wc -l ) 
     echo "Le dossier $dossier contient $nb_fichiers fichier(s) ."
 else
-    echo "Le dossier $dossier n'existe pas."
+    echo "Le dossier $dossier n'existe pas ."
 fi 
