0b43d09f85e17a7a08daa2042a2f63de559d5216
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-17 08:11:40 +0100

    commit

--- count_files.sh
+++ count_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Demander le nom du dossier à l'utilisateur
-echo "Entrez le nom du dossier :"
+echo "Entrez le nom du dossier:"
 read dossier
 
 # Vérifie si le dossier existe
 
 if [ -d "$dossier" ]; then 
-    nb_fichiers=$(find "$dossier" -maxdepth 1 -type f | wc -l | xargs)
+    nb_fichiers=$(ls "$dossier" | wc -l | xargs)
     echo "Le dossier $dossier contient $nb_fichiers fichier(s)."
 else
     echo "Le dossier $dossier n'existe pas."
