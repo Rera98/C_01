2b81eb9296ebc27e6a3092200c0d6e11bf43ea60
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-19 18:24:13 +0100

    commit

--- find_string.sh
+++ find_string.sh
@@ -4,7 +4,7 @@
 echo "Entrez le nom du fichier :"
 read fichier
 
-# Vérifier si le fichier existe
+# Vérifie si le fichier existe
 if [ ! -f "$fichier" ]; then
     echo "Le fichier '$fichier' n'existe pas."
     exit 1
@@ -17,7 +17,7 @@ read chaine
 # Utiliser grep pour chercher la chaîne dans le fichier
 # -q : mode silencieux (ne rien afficher, mais retourne un code de sortie)
 # -F : recherche de la chaîne telle quelle (pas une expression régulière)
-if grep -qF "$chaine" "$fichier"; then
+if grep -q "$chaine" "$fichier"; then
     echo "La chaîne '$chaine' a été trouvée dans $fichier."
 else
     echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
