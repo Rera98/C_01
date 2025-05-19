707ddd4d9028150c8ba8638e798e92326afc433b
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-20 00:49:28 +0100

    commit

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Initialiser un compteur
-i=1
-# Boucle sur tous les arguments
+# Affiche tous les arguments en une seule fois
 for arg in "$@"; do
-    echo " Argument $i: $arg "
-    shift             # Décale tous les arguments vers la gauche
-    ((i++))    # Incrémente le compteur
+    echo -n "$arg "
+done
+
+# Traitement individuel avec \$1 et shift
+i=1
+# echo
+while [ "$#" -gt 0 ]; do
+    echo "Argument $i: $1"
+    shift
+    ((i++))
 done
