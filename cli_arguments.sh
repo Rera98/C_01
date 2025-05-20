527b8f95a1ca46e028b968fcb327ca10675a7f06
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-20 07:36:42 +0100

    commit

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Affiche tous les arguments en une seule fois
-#for arg in "$@"; do
-    echo "$@"
-#done
-
+echo "$@" 
 # Traitement individuel avec \$1 et shift
-i=1
-# echo
+i=1 
+# Boucle sur les arguments et les affiches un a un
 while [ "$#" -gt 0 ]; do
     echo "Argument $i: $1"
     shift
