53b95439a062ce293937cd5974991338ca4897d3
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-20 08:47:15 +0100

    commit

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -5,11 +5,11 @@ echo "$@"
 # Traitement individuel avec \$1 et shift
 i=1 
 # Boucle sur les arguments et les affiches un a un
- [ "$#" -gt 0 ];
+ #[ "$#" -gt 0 ];
     echo "Argument $i: $1"
     shift
     ((i++))
-[ "$#" -gt 0 ];
+#[ "$#" -gt 0 ];
     echo "Argument $i: $1"
     shift
     ((i++))
